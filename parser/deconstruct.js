e2b6bcfe289e862a0f803a20c3b1fb972407d720
amprew <me@andrewww.co.uk>
2021-11-19 14:39:13 +0000

    Add initial regex check.

--- parser/deconstruct.js
+++ parser/deconstruct.js
@@ -1,4 +1,13 @@
+// trivial regex to match spaces between regex parts.
+// would need more investment in initial check.
+const cronJobRegex = /.+(\s.+){5}/;
+
 export const deconstruct = (cronString) => {
+  if (!cronJobRegex.test(cronString))
+    throw new Error(
+      'cron job did not match format of "minute hour dayOfMonth month dayOfWeek command"'
+    );
+
   const [minute, hour, dayOfMonth, month, dayOfWeek, ...command] =
     cronString.split(" ");
 
